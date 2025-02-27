054047c8299b19d796be1b64d339b3a983b79a10
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-02-27 12:00:55 +0530

    Converting Temperature from celsius to fahrenheit

---
+++ TemperatureConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ public class TemperatuereConversion{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Temperature in Celsius :");
+	int temperatureinCelsius = sc.nextInt();
+	
+	int temperatureinFahrenheit = (temperatureinCelsius * 9/5) + 32;
+	
+	System.out.println("Temperature in Fahrenheit is"  + temperatureinFahrenheit);
+	
+	sc.close();
+	
+	}
+	}
